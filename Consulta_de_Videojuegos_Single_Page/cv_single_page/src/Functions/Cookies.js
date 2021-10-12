946b5d2e46beaebb970c59e6f30624b3a3ae60f2
JavierZavala17 <javier_z_t@hotmail.com>
2021-10-12 15:05:24 -0500

    Update Cookies.js

--- Consulta_de_Videojuegos_Single_Page/cv_single_page/src/Functions/Cookies.js
+++ Consulta_de_Videojuegos_Single_Page/cv_single_page/src/Functions/Cookies.js
@@ -1,11 +1,10 @@
 export function setCookie (key, value) {
     
-    localStorage.setItem(key, value)
-    return value;
+    return (localStorage.setItem(key, JSON.stringify(value)));
 }
 
 export function getCookie(key) {
     
-    return  localStorage.getItem(key);
+    return JSON.parse(localStorage.getItem(key))||false;
   
 }
