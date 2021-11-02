95b8b774a85e6932b0ca4924bb7c19f3fc31d1d9
zambranoconz <riczam2797@gmail.com>
2021-11-02 13:37:20 -0600

    Intento de volver al merge de rudy

--- Consulta_de_Videojuegos_Single_Page/cv_single_page/src/ConsultaVideojuegosSinglePageApp.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { MainAppRouter } from './componentes/routers/MainAppRouter';
-export const ConsultaVideojugosSinglePageApp = () => {
-    return (
-        <MainAppRouter />
-    )
-}
