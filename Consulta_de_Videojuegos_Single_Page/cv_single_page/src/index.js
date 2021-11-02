8db4dd9b32da0b8755614d24e1f8a877a9a835f8
zambranoconz <riczam2797@gmail.com>
2021-11-02 12:34:20 -0600

    Toda la practica
    hasta el final de los pdf's del profe

--- Consulta_de_Videojuegos_Single_Page/cv_single_page/src/index.js
+++ Consulta_de_Videojuegos_Single_Page/cv_single_page/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MainAppRouter } from './componentes/routers/MainAppRouter';
 import { ConsultaVideojugosSinglePageApp } from './ConsultaVideojuegosSinglePageApp';
+import { MainApp } from './hooks/context/MainApp';
 
 ReactDOM.render(
-  <MainAppRouter />,
+  <MainApp />,
   document.getElementById('root')
 );
 
