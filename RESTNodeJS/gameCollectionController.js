8010f999039043c346d6cd6dc458c0a5397b8928
Javier Zavala <48965617+JavierZavala17@users.noreply.github.com>
2021-11-08 20:51:07 -0600

    Tarea 02 11 (#18)
    DELETE METHOD
    "SEARCH" METHOD

--- RESTNodeJS/gameCollectionController.js
+++ RESTNodeJS/gameCollectionController.js
@@ -1,9 +1,10 @@
 'use strict';
 var fs = require("fs");
 module.exports.obtener_juegos = function (req, res) {
+    console.log("ewe");
     fs.readFile(__dirname + "/" + "juegos.json", 'utf8', function (err, data) {
-        console.log(err);
-        console.log(data);
+        // console.log(err);
+        // console.log(data);
         res.end(data);
     });
 };
@@ -11,12 +12,12 @@ module.exports.obtener_juegos = function (req, res) {
 module.exports.agregar_juego = function (req, res) {
     fs.readFile(__dirname + "/" + "juegos.json", 'utf8', function (err, data) {
         var array = JSON.parse(data);
-        console.log(err);
-        console.log(data);
+        // console.log(err);
+        // console.log(data);
         var nuevo = req.body;
         array.push(nuevo);
         fs.writeFile(__dirname + "/" + "juegos.json", JSON.stringify(array), 'utf8', function (err, data) {
-            console.log(err);
+            // console.log(err);
             res.end(err);
         });
         res.end(JSON.stringify(array));
@@ -26,7 +27,31 @@ module.exports.obtener_juego = function (req, res) {
     fs.readFile(__dirname + "/" + "juegos.json", 'utf8', function (err, data) {
         var juegos = JSON.parse(data);
         var juego = juegos[req.params.gameIndex]
-        console.log(juego);
+        // console.log(juego);
         res.end(JSON.stringify(juego));
+    });
+};
+
+module.exports.borrar_juego = function (req, res) {
+    fs.readFile(__dirname + "/" + "juegos.json", "utf8", function (err, data) {
+        var array = JSON.parse(data);
+        console.log(data);
+        var juego_existe = req.body.id;
+        
+        array = array.filter(juego => juego.id !== juego_existe);
+        fs.writeFile(__dirname + "/" + "juegos.json", JSON.stringify(array), "utf8", function(err, data) {
+            res.end(err);
+        });
+        res.end(JSON.stringify(array));
+    });
+};
+
+module.exports.buscar_juego = function (req, res) {    
+    fs.readFile(__dirname + "/" + "juegos.json", 'utf8', function (err, data) {
+        var nombre = req.query.nombre;
+        var array = JSON.parse(data);
+        
+        array = array.filter( game => game.nombre.toLowerCase().indexOf(nombre.toLowerCase()) !== -1)
+        res.end(JSON.stringify(array));
     });
 };
